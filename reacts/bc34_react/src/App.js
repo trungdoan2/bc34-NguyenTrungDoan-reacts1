27ee05a2a219508a8c2bcc69fef11ab2ec2774f8
trungdoan2 <108280266+trungdoan2@users.noreply.github.com>
2022-09-17 11:28:29 +0700

    Add files via upload

---
+++ reacts/bc34_react/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import DemoComponent from './component/demobuoimot/DemoComponent';
+import BaiTapLayOut from './component/demo_layout/BaiTapLayOut';
+import DemoState from './component/demoState/DemoState';
+import BaiTapCar from './component/bt_car_basic/BaiTapCar';
+import BaiTapMovie from './component/bt_car_basic/BaiTapMovie';
+import BaiTapThucHanhLayOut from './component/BaiTapLayOutComponent/BaiTapThucHanhLayOut';
+
+function App() {
+  return (
+    <div className="App">
+       {/* <DemoComponent /> */}
+        {/* <BaiTapLayOut /> */}
+        {/* <DemoComponent /> */}
+        {/* <DemoState  /> */}
+        {/* <BaiTapCar /> */}
+        {/* <BaiTapMovie /> */}
+        <BaiTapThucHanhLayOut />
+    </div>
+  );
+}
+
+export default App;
